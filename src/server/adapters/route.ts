bb375408b693b3d0250bf9884a78c8d06873bb02
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2024-08-07 15:04:14 -0300

    feat: update express route adapter

--- src/server/adapters/route.ts
+++ src/server/adapters/route.ts
@@ -3,7 +3,11 @@ import { IController } from '../../application/interfaces/IController';
 
 export function routeAdapter(controller: IController) {
   return async (request: Request, response: Response) => {
-    const { statusCode, body } = await controller.handle({ body: request.body });
+    const { statusCode, body } = await controller.handle({
+      accountId: request.metadata.accountId,
+      body: request.body,
+      params: request.params,
+    });
 
     response.status(statusCode).json(body);
   };
