94195b485433a3ca76ded8f7140eb161428da0a6
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2024-08-07 13:51:44 -0300

    refactor: create express route adapter

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,26 +1,14 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { makeSignInController } from '../factories/makeSignInController';
 import { makeSignUpController } from '../factories/makeSignUpController';
+import { routeAdapter } from './adapters/route-adapter';
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/sign-up', async (request: Request, response: Response) => {
-  const controller = makeSignUpController();
-
-  const { statusCode, body } = await controller.handle({ body: request.body });
-
-  response.status(statusCode).json(body);
-});
-
-app.post('/sign-in', async (request: Request, response: Response) => {
-  const controller = makeSignInController();
-
-  const { statusCode, body } = await controller.handle({ body: request.body });
-
-  response.status(statusCode).json(body);
-});
+app.post('/sign-up', routeAdapter(makeSignUpController()));
+app.post('/sign-in', routeAdapter(makeSignInController()));
 
 app.listen(3001, () => {
   console.log('Server started at http://localhost:3001');
