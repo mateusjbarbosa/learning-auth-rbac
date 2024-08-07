60db228df652cd1a1973a69d82a2113a60e7d8f8
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2024-08-07 10:14:45 -0300

    feat: create sign up usecase

---
+++ src/application/usecases/sign-up.ts
@@ -0,0 +1,33 @@
+import { hash } from "bcryptjs"
+import { prismaClient } from "../../database/prismaClient"
+import { AccountAlreadyExists } from "../errors/accounts-already-exists"
+
+type Input = {
+  name: string
+  email: string
+  password: string
+}
+
+type Output = void
+
+export class SignUpUsecase {
+  async execute({ email, name, password }: Input): Promise<Output> {
+    const accountAlreadyExists = await prismaClient.account.findUnique({
+      where: { email }
+    })
+
+    if (accountAlreadyExists) {
+      throw new AccountAlreadyExists();
+    }
+
+    const hashedPassword = await hash(password, 10)
+
+    await prismaClient.account.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword
+      }
+    })
+  }
+}
