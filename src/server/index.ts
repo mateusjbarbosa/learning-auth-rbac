c7a216a0e1cd2767c13c2ca7651092d6cc19fd71
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2024-08-07 15:07:55 -0300

    feat: create list users express route

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
+import { makeAuthenticationMiddleware } from '../factories/makeAuthenticationMiddleware';
+import { makeListUsersController } from '../factories/makeListUsersController';
 import { makeSignInController } from '../factories/makeSignInController';
 import { makeSignUpController } from '../factories/makeSignUpController';
+import { middlewareAdapter } from './adapters/middleware';
 import { routeAdapter } from './adapters/route';
 
 const app = express();
@@ -9,6 +12,11 @@ app.use(express.json());
 
 app.post('/sign-up', routeAdapter(makeSignUpController()));
 app.post('/sign-in', routeAdapter(makeSignInController()));
+
+app.get('/users',
+  middlewareAdapter(makeAuthenticationMiddleware()),
+  routeAdapter(makeListUsersController())
+);
 
 app.listen(3001, () => {
   console.log('Server started at http://localhost:3001');
