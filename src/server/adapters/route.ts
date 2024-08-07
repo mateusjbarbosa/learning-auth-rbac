32d154bcbb651371c0653b89bee6d4e597c5884c
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2024-08-07 15:21:59 -0300

    fix: update express request metadata

--- src/server/adapters/route.ts
+++ src/server/adapters/route.ts
@@ -4,7 +4,7 @@ import { IController } from '../../application/interfaces/IController';
 export function routeAdapter(controller: IController) {
   return async (request: Request, response: Response) => {
     const { statusCode, body } = await controller.handle({
-      accountId: request.metadata.accountId,
+      accountId: request.metadata?.accountId,
       body: request.body,
       params: request.params,
     });
