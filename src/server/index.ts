6569c6111a4b5a161852a12115750a1f29c88879
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2024-08-07 11:24:55 -0300

    feat: create sign in http server route

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
+import { SignInController } from '../application/controllers/sign-in';
 import { SignUpController } from '../application/controllers/sign-up';
+import { SignInUsecase } from '../application/usecases/sign-in';
 import { SignUpUsecase } from '../application/usecases/sign-up';
 
 const app = express()
@@ -9,6 +11,15 @@ app.use(express.json())
 app.post('/sign-up', async (request: Request, response: Response) => {
   const usecase = new SignUpUsecase()
   const controller = new SignUpController(usecase)
+
+  const { statusCode, body } = await controller.handle({ body: request.body })
+
+  response.status(statusCode).json(body)
+})
+
+app.post('/sign-in', async (request: Request, response: Response) => {
+  const usecase = new SignInUsecase()
+  const controller = new SignInController(usecase)
 
   const { statusCode, body } = await controller.handle({ body: request.body })
 
