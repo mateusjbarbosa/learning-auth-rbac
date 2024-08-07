a409a46740b89c094e787b509f4cf6263056aa34
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2024-08-07 11:20:03 -0300

    feat: create sign up http server route

---
+++ src/server/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import { SignUpController } from 'src/application/controllers/sign-up';
+import { SignUpUsecase } from '../application/usecases/sign-up';
+
+const app = express()
+
+app.use(express.json())
+
+app.post('/sign-up', async (request: Request, response: Response) => {
+  const usecase = new SignUpUsecase()
+  const controller = new SignUpController(usecase)
+
+  const { statusCode, body } = await controller.handle({ body: request.body })
+
+  response.status(statusCode).json(body)
+})
+
+app.listen(3001, () => {
+  console.log('Server started at http://localhost:3001')
+})
